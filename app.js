a1ddc192836c89789655e6a2b7623396b2dd0c04
farzana-shahid <shahidkrorpati@gmail.com>
2024-10-04 00:50:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var mart = new Friut("mango" , " yellow", "100",);
+function Friut (name, color , price){
+    this.name ="mango";
+    this.color = "yellow";
+    this.price = 100
+};
+console.log(mart)
+
+var mart = new Vegetable ("ladyfinger", "green", 300);
+function Vegetable (name , color , price){
+    this.name = "ladyfnger";
+    this.color = "green";
+    this.price = 300
+};
+console.log(mart)
+
+var mart = new Icecream ( "vanilla", "2scope",500);
+function Icecream (flavor , quantity, price){
+    this.flavor ="vanilla";
+    this.quantity ="2scope";
+    this. price = 500
+};
+console.log(mart)
+
+var mart = new Pizza ("fajitta","large",1200);
+function Pizza (varaity, size,price){
+this.varaity = "fajitta";
+ this.size = "large";
+ this.price = 1200 ;
+};
+console.log(mart) 
+
+var mart = new Drink ("milk",300, "1liter");
+function Drink (name , price , quantity ){
+this.name = "milk";
+this.price = 300 ;
+this.quantity= "1liter";
+};
+console.log(mart)
+ 
